d0ecddd50e370530671fd2e73398a9e4c0651cee
songahh <gunsong2@gmail.com>
2023-01-27 19:03:25 +0900

    [flutter] 05 홈 화면에 원형 이미지 / 사각형 이미지 슬라이더 위젯 추가하기

--- front/lib/screen/home_screen.dart
+++ front/lib/screen/home_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:front/widget/carousel_slider.dart';
 
 import '../model/model_movie.dart';
+import '../widget/circle_slider.dart';
+import '../widget/box_slider.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
@@ -9,6 +11,18 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<Movie> movies = [
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'test_movie_1.png',
+      'like': false
+    }),
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'test_movie_1.png',
+      'like': false
+    }),
     Movie.fromMap({
       'title': '사랑의 불시착',
       'keyword': '사랑/로맨스/판타지',
@@ -48,6 +62,12 @@ class _HomeScreenState extends State<HomeScreen> {
           CarouselImage(movies: movies),
           TopBar(),
         ],
+      ),
+      CircleSlider(
+        movies: movies,
+      ),
+      BoxSlider(
+        movies: movies
       )
     ]);
   }
